f3481d51ddb891fb3f7e4382bcb3caee133467c3
Lucas Ferreira <lucas.santos61@fatec.sp.gov.br>
2018-03-25 19:42:56 -0300

    Add new feature : Complete tasks

---
+++ src/CompleteCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tasks;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class CompleteCommand extends Command
+{
+    public function configure()
+    {
+        $this->setName('complete')
+            ->setDescription('Complet a task')
+            ->addArgument('id',InputArgument::REQUIRED);
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $id = $input->getArgument('id');
+
+        $this->database->query('delete from tasks where id = :id',compact('id'));
+
+        $output->writeln('<info>Task Complete !</info>');
+
+        $this->showTasks($output);
+    }
+}
